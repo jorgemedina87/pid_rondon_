fbe68c0b917b009665b613055c5832e7a0775b11
jorgemedina87 <jorgemedina.herrada@gmail.com>
2022-12-12 11:32:42 -0500

    update

--- server.R
+++ server.R
@@ -18,11 +18,18 @@ library(readxl)
 library(plotly)
 library(DT)
 library(tidyr)
+library(rio)
 
 ### data   
 
-bd_perfil_pozo_v <- read_excel(file.path("data/bd_cr_c_nf_nn.xlsx"), sheet = "Sheet1")
-bd_perfil_pozo_ <- read_excel(file.path("data/bd_perfil_pozo_.xlsx"), sheet = "Sheet1")
+#bd_perfil_pozo_v <- read_excel(file.path("data/bd_cr_c_nf_nn.xlsx"), sheet = "Sheet1")
+
+#bd_perfil_pozo_v <- rio::import("https://github.com/jorgemedina87/pid_rondon_/raw/main/data/bd_cr_c_nf_nn.xlsx")
+
+#bd_perfil_pozo_ <- read_excel(file.path("data/bd_perfil_pozo_.xlsx"), sheet = "Sheet1")
+
+bd_perfil_pozo_ <- rio::import("https://github.com/jorgemedina87/pid_rondon_/raw/main/data/bd_perfil_pozo_.xlsx")
+
 
 bd_WO<-  read.csv(file.path("data/bd_wo.csv"))
 
@@ -37,10 +44,9 @@ bd_prueba_b_pp <-  read.csv(file.path("data/bd_prueba_b_pp.csv"))
 rawdata_total <-  read.csv(file.path("data/bd_total.csv"))
 
 
-id_campo <-bd_perfil_pozo_ %>%
-  select(well,CAMPO)%>%
-  unique()
-
+# id_campo <-bd_perfil_pozo_ %>%
+#   select(well,CAMPO)%>%
+#   unique()
 
 
 # SERVER ------------------------------------------------------------------
@@ -49,7 +55,7 @@ id_campo <-bd_perfil_pozo_ %>%
 
 server <- function(input, output, session) {
 
-  # source("modules/server_perfil.R", local = TRUE)
+  #source("modules/server_perfil.R", local = TRUE)
   source("modules/server_vpn_total.R", local = TRUE)
   source("modules/server_Well_Well_Basic.R", local = TRUE)
   source("modules/server_VPN_WO.R", local = TRUE)
